d28396fe5482ba7551466458f8fbb36925a4d23a
yinyong <yinyong@sogou-inc.com>
2014-07-30 10:47:33 +0800

    loginPage done

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+var methodOverride = require('method-override');
+var serveStatic = require('serve-static');
+var errorhandler = require('errorhandler');
+
+// all environments
+app.set('port', process.env.PORT || 3030);
+app.set('views', path.join(__dirname, 'template'));
+//app.use(express.favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(methodOverride('X-HTTP-Method-Override'));
+//app.use(app.router);
+app.use(serveStatic('.'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(errorhandler())
+}
+
+app.get('/', function(req, res, next) {
+    return res.send('use .action');
+});
+
+app.get(/(.+)\.action$/, function(req, res) {
+    var stub = RegExp.$1;
+    return fs.readFile(path.join('template', stub + '.html'), {
+        encoding: 'utf-8'
+    }, function(err, content) {
+        if (err) {
+            return res.send(404);
+        }
+        return res.send(content);
+    });
+})
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ static/js/page.js
@@ -0,0 +1,1 @@
+define("page",["jquery","common"],function(a){var b=a("jquery");b(function(){var c=a("common"),d=b("#pg-server-message");if(d.length>0){var e=d.data("status"),f=d.data("message"),g={message:f};""!==e&&(0===e||"0"===e?g.success=!0:g.error=!0,c.showMessage(g))}var h=b("#title-detail"),i=b("title").html();h.length>0&&b("title").html(h.html()+" -"+i),c.initPoptips()})});
