a7f907b902be72d6f0705c6ddc2fcda692c52a95
yinyong <yinyong@sogou-inc.com>
2014-07-30 13:00:18 +0800

    fixed 404 output

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /**
-  * Copyright (C) 2014 yanni4night.com
-  * app.js
-  *
-  * changelog
-  * 2014-07-30[11:28:33]:authorized
-  *
-  * @author yanni4night@gmail.com
-  * @version 0.1.0
-  * @since 0.1.0
-  */
+ * Copyright (C) 2014 yanni4night.com
+ * app.js
+ *
+ * changelog
+ * 2014-07-30[11:28:33]:authorized
+ *
+ * @author yanni4night@gmail.com
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 
 var express = require('express');
 var http = require('http');
@@ -25,34 +25,35 @@ app.set('port', process.env.PORT || 3030);
 app.set('views', path.join(__dirname, 'template'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(serveStatic('.'));
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(errorhandler())
+  app.use(errorhandler())
 }
 
 app.get('/', function(req, res, next) {
-    return res.redirect('index.action');
+  return res.redirect('index.action');
 });
 
 //验证码
 app.get('/image.jsp', function(req, res, next) {
-    return res.redirect('/static/img/captcha.png');
+  return res.redirect('/static/img/captcha.png');
 });
 
 app.get(/(.+)\.action$/i, function(req, res) {
-    var stub = String(RegExp.$1).toLowerCase();
-    return fs.readFile(path.join('template', stub + '.html'), {
-        encoding: 'utf-8'
-    }, function(err, content) {
-        return res.send(err?404:content);
-    });
+  var stub = String(RegExp.$1);
+  var file = path.join('template', stub + '.html');
+  return fs.readFile(file, {
+    encoding: 'utf-8'
+  }, function(err, content) {
+    return res.send(err ? file + ' Not Found' : content);
+  });
 })
 
 http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
