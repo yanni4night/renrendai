1bc35c61c385d2202d96a5f75cf35ba3aa9f3b9b
yinyong <yinyong@sogou-inc.com>
2014-07-30 11:29:01 +0800

    update readme;clean app.js

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ renrendai
 =========
 
 A copy of renrendai
+
+usage
+=========
+
+1. Install [nodejs](http://nodejs.org);
+2. Git clone to your workspace;
+3. `npm install` and waiting for installing complete;
+4. `node app.js`,your local server at port:3030 should startup now.
+
+license
+=========
+GPLv3,just for studying. [renrendai.com](http://www.renrendai.com) all rights reserved.
--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 /**
- * Module dependencies.
- */
+  * Copyright (C) 2014 yanni4night.com
+  * app.js
+  *
+  * changelog
+  * 2014-07-30[11:28:33]:authorized
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
 
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
-var app = express();
 var methodOverride = require('method-override');
 var serveStatic = require('serve-static');
 var errorhandler = require('errorhandler');
 
+var app = express();
 // all environments
 app.set('port', process.env.PORT || 3030);
 app.set('views', path.join(__dirname, 'template'));
-//app.use(express.favicon());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(methodOverride('X-HTTP-Method-Override'));
-//app.use(app.router);
 app.use(serveStatic('.'));
 
 if (process.env.NODE_ENV === 'development') {
@@ -37,15 +44,12 @@ app.get('/image.jsp', function(req, res, next) {
     return res.redirect('/static/img/captcha.png');
 });
 
-app.get(/(.+)\.action$/, function(req, res) {
-    var stub = RegExp.$1;
+app.get(/(.+)\.action$/i, function(req, res) {
+    var stub = String(RegExp.$1).toLowerCase();
     return fs.readFile(path.join('template', stub + '.html'), {
         encoding: 'utf-8'
     }, function(err, content) {
-        if (err) {
-            return res.send(404);
-        }
-        return res.send(content);
+        return res.send(err?404:content);
     });
 })
 
