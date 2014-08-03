f0ced5ab22562c25d5177929b49149edcc82dd5f
yinyong <yinyong@sogou-inc.com>
2014-08-03 17:08:19 +0800

    velocity supported

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,10 @@
  *
  * changelog
  * 2014-08-01[18:41:37]:authorized
+ * 2014-08-03[17:09:39]:velocity supported
  *
  * @author yanni4night@gmail.com
- * @version 0.1.0
+ * @version 0.1.1
  * @since 0.1.0
  */
 
@@ -18,59 +19,73 @@ swig.setDefaults({
 });
 
 module.exports = function(grunt) {
-
+    var SVN = "../duonet/p2p-duonet-web/src/main/webapp/";
+    var SVN_STATIC = SVN + "assets/";
+    var SVN_VM = SVN + "WEB-INF/vm/";
     var BUILD = "build/";
     var TPL = "template/";
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         copy: {
-            fonts:{
-                src:['static/fonts/*.woff'],
-                dest:BUILD
+            fonts: {
+                src: ['static/fonts/*.woff'],
+                dest: BUILD
             },
-            img:{
-                expand:true,
-                cwd:'.',
-                src:['static/img/**/*.{gif,png,jpg,jpeg}'],
-                dest:BUILD
+            img: {
+                expand: true,
+                cwd: '.',
+                src: ['static/img/**/*.{gif,png,jpg,jpeg}'],
+                dest: BUILD
+            },
+            pub_static: {
+                expand: true,
+                cwd: BUILD,
+                src: ['static/**/*'],
+                dest: SVN_STATIC
+            },
+            pub_vm: {
+                expand: true,
+                cwd: BUILD + TPL,
+                src: ['**/*.vm'],
+                dest: SVN_VM
             }
         },
-        cssmin:{
-            css:{
-                expand:true,
-                cwd:'.',
-                src:['static/**/*.css'],
-                dest:BUILD
+        cssmin: {
+            css: {
+                expand: true,
+                cwd: '.',
+                src: ['static/**/*.css'],
+                dest: BUILD
             }
         },
         clean: {
-            all:[BUILD]
+            all: [BUILD]
         },
         stamp: {
-            options:{
-                baseDir:BUILD,
-                ignoreMissing:false
+            options: {
+                baseDir: BUILD,
+                ignoreMissing: false
             },
-            html:{
-                expand:true,
-                cwd:BUILD,
-                src:['static/css/**/*.css'],
-                dest:BUILD
+            html: {
+                expand: true,
+                cwd: BUILD,
+                src: ['static/css/**/*.css'],
+                dest: BUILD
             },
-            css:{
-                expand:true,
-                cwd:BUILD,
-                src:['template/**/*.html'],
-                dest:BUILD
+            css: {
+                expand: true,
+                cwd: BUILD,
+                src: ['template/**/*.vm'],
+                dest: BUILD
             }
         },
         uglify: {
-            js:{
-                expand:true,
-                cwd:'.',
-                src:['static/js/**/*.js'],
-                dest:BUILD
+            js: {
+                expand: true,
+                cwd: '.',
+                src: ['static/js/**/*.js'],
+                dest: BUILD
             }
         },
         swig: {
@@ -78,24 +93,26 @@ module.exports = function(grunt) {
                 expand: true,
                 cwd: TPL,
                 src: ['**/*.html'],
-                dest: BUILD + TPL
+                dest: BUILD + TPL,
+                ext: '.vm'
             }
         }
     });
 
     grunt.registerMultiTask('swig', 'render', function() {
         this.files.forEach(function(f) {
-            var src = f.src[0].replace(TPL,'');
-            grunt.file.write(f.dest,swig.renderFile(src,{}));
+            var src = f.src[0].replace(TPL, '');
+            grunt.file.write(f.dest, swig.renderFile(src, {}));
             grunt.log.ok(f.dest);
         });
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-yui-compressor');//cssmin cannot handle csshack
+    grunt.loadNpmTasks('grunt-yui-compressor'); //cssmin cannot handle csshack
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-web-stamp');
 
-    grunt.registerTask('default', ['clean','copy','cssmin','uglify','swig','stamp']);
+    grunt.registerTask('default', ['clean', 'copy:fonts', 'copy:img', 'cssmin', 'uglify', 'swig', 'stamp']);
+    grunt.registerTask('pub', ['default', 'copy:pub_static', 'copy:pub_vm']);
 };
